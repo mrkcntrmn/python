9df4d1447820e3d1b14a6c00293d03e5dd199f21
mrkcntrmn <ilovepoker@gmail.com>
2018-10-08 14:28:56 -0500

    hello world

---
+++ Hello.py
@@ -0,0 +1,19 @@
+'''
+Created on Mar 4, 2017
+
+@author: MARK
+'''
+
+#This is a comment using Python!
+
+
+#This is how you print output using Python version 2.X
+
+print "Hello world!"
+
+
+#This is how you print output using Python version 3.X
+
+print("Hello world!")
+
+print("both versions work in 2.x so use 2.x... duh!")
